7d46d94b353c5b6ac61d0da0035585d38201d7a6
Larisa Varentsova <lvarentsova@sportmaster.ru>
2021-09-26 19:28:22 +0300

    Added file tests

--- src/test/java/guru/qa/SelenideFilesTest.java
+++ src/test/java/guru/qa/SelenideFilesTest.java
@@ -1,32 +1,17 @@
 package guru.qa;
 
 import com.codeborne.pdftest.PDF;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.FileDownloadMode;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.xlstest.XLS;
-import com.opencsv.CSVReader;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.ArrayUtils;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.FileHeader;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.junit.jupiter.api.Test;
-import org.opentest4j.AssertionFailedError;
 
-import javax.annotation.Nonnull;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -35,73 +20,56 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SelenideFilesTest {
 
     @Test
-    void downloadFileTest(String arg) throws Exception {
-        open("https://github.com/selenide/selenide/blob/master/README.md");
-        File download = $("#raw-url").download();
+    void parseTxtFileTest() throws Exception {
         String result;
-
-        try (InputStream is = new FileInputStream(download)) {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("test.txt")) {
             result = new String(is.readAllBytes(), "UTF-8");
         }
-        assertThat(result).contains("Selenide = UI Testing Framework powered by Selenium WebDriver");
+        assertThat(result).contains("Достоевский");
     }
 
     @Test
-    void uploadFileTest() {
-        open("https://the-internet.herokuapp.com/upload");
-        $("input[type='file']").uploadFromClasspath("example.txt");
-        $("#file-submit").click();
-        $("#uploaded-files")
-                .shouldHave(text("example.txt"));
-    }
-
-    @Test
-    void downloadPdfTest() throws Exception {
-        open("https://junit.org/junit5/docs/current/user-guide/");
-        File download = $(byText("PDF download")).download();
+    void parsePdfTest() throws Exception {
+        open("https://all-the-books.ru/books/dostoevskiy-fedor-idiot/");
+        File download = $(byText("Скачать Идиот в PDF")).download();
         PDF parsed = new PDF(download);
-        assertThat(parsed.author).contains("Marc Philipp");
+        assertThat(parsed.author).contains("Федор Михайлович Достоевский");
+        assertThat(parsed.text).contains("Идиот");
     }
 
     @Test
-    void downloadExcelTest() throws Exception {
-//        Selenide.open("https://junit.org/junit5/docs/current/user-guide/");
-//        File download = $(byText("PDF download")).download();
-        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("175.xlsx")) {
+    void parseExcelTest() throws Exception {
+//        Selenide.open("https://okeanbusin.ru/shop/zakaz-cherez-excel-fayly/");
+//        File download = $(byText("Чешские бусины, разные, фасовка 10 гр.")).download();
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("zakaz.xlsx")) {
             XLS parsed = new XLS(stream);
-            assertThat(parsed.excel.getSheetAt(1).getRow(4).getCell(1).getStringCellValue())
-                    .isEqualTo("Алешина Ольга Валентиновна");
+            assertThat(parsed.excel.getSheetAt(0).getRow(7).getCell(0).getStringCellValue())
+                    .isEqualTo("Изображение");
         }
     }
 
     @Test
-    void parseCsvTest() throws Exception {
-        URL url = getClass().getClassLoader().getResource("file.csv");
-        CSVReader reader = new CSVReader(new FileReader(new File(url.toURI())));
-
-        List<String[]> strings = reader.readAll();
-
-        assertThat(strings).contains(
-                new String[] {"lector", "lecture"},
-                new String[] {"Tuchs", "JUnit"},
-                new String[] {"Eroshenko", "Allure"}
-        );
+    void parseDocTest() throws Exception {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("test.docx")) {
+            XWPFDocument docxFile = new XWPFDocument(is);
+            List<XWPFParagraph> paragraphs = docxFile.getParagraphs();
+            assertThat(paragraphs.get(0).getParagraphText().contains("Достоевский"));
+        }
     }
 
     @Test
-    void parseZipFileTest() throws Exception {
-        try (InputStream is = getClass().getClassLoader().getResourceAsStream("zip_2MB.zip")) {
-            ZipInputStream zis = new ZipInputStream(is);
-            ZipEntry entry;
+    void zipFileTest() throws Exception {
+        ZipFile zipFile = new ZipFile("./src/test/resources/response.zip");
 
-            while((entry = zis.getNextEntry()) != null) {
-                System.out.println(entry.getName());
-            }
- //           for text zipped files
-//            Scanner sc = new Scanner(zis);
-//            while (sc.hasNext()) {
-//                System.out.println(sc.nextLine());
-//            }
+        if (zipFile.isEncrypted()) {// Если установлен пароль
+            zipFile.setPassword("123456qQ");
+        }
+        zipFile.extractAll("./src/test/resources/extract");
+
+        List fileHeaderList = zipFile.getFileHeaders();
+        for (int i = 0; i < fileHeaderList.size(); i++) {
+            FileHeader fileHeader = (FileHeader) fileHeaderList.get(i);
+            assertThat(fileHeader.getFileName()).contains("response");
         }
     }
 }
